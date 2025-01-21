90ce78d25c91ca0a2db1a59caf398f9c865b456a
Niall Carroll <niallpcarroll@gmail.com>
2025-01-21 16:29:32 +0000

    Add sorting and product counts to product page

--- products/views.py
+++ products/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from django.db.models import Q 
+from django.db.models import Q
 from .models import Product, Category
 
 # Create your views here.
 
 def all_products(request):
-    """ A view to show individual product details """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
-    categories = None 
+    categories = None
     sort = None
     direction = None
 
@@ -21,13 +21,14 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
-            products = products.order_by(sortkey)        
-
+            products = products.order_by(sortkey)
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -38,11 +39,11 @@ def all_products(request):
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
-            products = products.filter(queries)    
+            products = products.filter(queries)
 
-    current_sorting = f'(sort)_(direction)'
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
@@ -55,7 +56,7 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
